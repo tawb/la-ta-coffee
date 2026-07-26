b37c5ebfc102cf89faa18ca1f42c67b8dbc150a0
Tawba Abdallah <tawba222@proton.me>
2026-07-26 09:58:29 +0300

    Add MenuService and Search modal with signal-based live filtering

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,9 +5,10 @@ import { LoginModalComponent } from './components/login-modal/login-modal.compon
 import { SignupModalComponent } from './components/signup-modal/signup-modal.component';
 import { ResetModalComponent } from './components/reset-modal/reset-modal.component';
 import { AccountModalComponent } from './components/account-modal/account-modal.component';
+import { SearchModalComponent} from './components/search-modal/search-modal.component';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,NavComponent,LoginModalComponent,SignupModalComponent, ResetModalComponent,AccountModalComponent],
+  imports: [RouterOutlet,NavComponent,LoginModalComponent,SignupModalComponent, ResetModalComponent,AccountModalComponent,SearchModalComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
