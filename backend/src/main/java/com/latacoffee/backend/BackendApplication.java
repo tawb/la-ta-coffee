b861e8af23887de6de718cb5c4211a92f4bb157f
Tawba Abdallah <tawba222@proton.me>
2026-08-20 11:32:52 +0300

    feat(menu): add MenuController with search endpoint and seed data

--- backend/src/main/java/com/latacoffee/backend/BackendApplication.java
+++ backend/src/main/java/com/latacoffee/backend/BackendApplication.java
@@ -1,7 +1,14 @@
 package com.latacoffee.backend;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.latacoffee.backend.menu.MenuCategory;
+import com.latacoffee.backend.menu.MenuCategoryRepository;
+import com.latacoffee.backend.menu.MenuItem;
+import com.latacoffee.backend.menu.MenuItemRepository;
 
 @SpringBootApplication
 public class BackendApplication {
@@ -9,5 +16,16 @@ public class BackendApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
 	}
+	@Bean
+	CommandLineRunner seedMenu(MenuCategoryRepository categoryRepo, MenuItemRepository itemRepo) {
+    	return args -> {
+			MenuCategory espresso = categoryRepo.save(new MenuCategory("Espresso"));
+			MenuCategory matcha = categoryRepo.save(new MenuCategory("Matcha"));
+
+			itemRepo.save(new MenuItem("espresso-cortado", espresso, "Cortado", "Equal parts", 14.0));
+			itemRepo.save(new MenuItem("espresso-flat-white", espresso, "Flat White", "Double shot, silky milk", 16.0));
+			itemRepo.save(new MenuItem("matcha-latte", matcha, "Matcha Latte", "Ceremonial grade, Uji", 22.0));
+		};
+}
 
 }
